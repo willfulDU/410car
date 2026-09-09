d962b711cacdd7977dfdfe368b400c87ded195cf
willfulDU <leo657290@gmail.com>
2026-09-09 11:04:18 +0800

    【屏幕】修复了挡位不显示的错误

--- User/main.c
+++ User/main.c
@@ -137,6 +137,7 @@ int main(void)//方向盘ecu
 			{
 				Motor_SetCW(0);
 				continue;
+			}
 
 			if (rx_buffer[5] != last_dnr)
 			{
@@ -170,7 +171,6 @@ int main(void)//方向盘ecu
 						OLED_ShowString(3, 9, "OFF  ");
 						break;
 				}
-			}
 			}
 
 			wheel_value = ((uint16_t)rx_buffer[1] << 8) | rx_buffer[2];
